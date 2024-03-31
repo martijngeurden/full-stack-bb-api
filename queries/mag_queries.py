2fde22d1342ed0ed892a571d691b2274d434ff04
martijngeurden <r0989527@student.thomasmore.be>
2024-03-31 20:33:43 +0200

    score value added & search allowed on this --- cleanup

--- queries/mag_queries.py
+++ queries/mag_queries.py
@@ -2,4 +2,4 @@ sub_all = "SELECT * FROM bakery.subscriptions;"
 
 sub_push = "INSERT INTO bakery.subscriptions (email, firstName, lastName, subscriptionType, address, address2, zipCode, city, additionalInformation) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
 
-pastries_all = "SELECT * FROM bakery.pastries WHERE score > %s;"
+pastries = "SELECT * FROM bakery.pastries WHERE score > %s;"
