a440741a61b757b9b222f82c85d80fefd543dc2f
martijngeurden <r0989527@student.thomasmore.be>
2024-04-08 22:40:20 +0200

    search parameters have been added

--- main.py
+++ main.py
@@ -68,11 +68,10 @@ def create_sub(sub: mag_models.model_subscription):
 
 
 @app.get("/get/pastries")
-def get_pastries(rating: int = 0, order: str = "ASC"):
+def get_pastries(rating: int = 0):
     query = mag_queries.pastries
     pastries = database.execute_sql_query(query, (
         rating,
-        order,
     ))
     if isinstance(pastries, Exception):
         return pastries, 500
